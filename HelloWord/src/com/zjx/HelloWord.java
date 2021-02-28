68b9d10cf1921a589b02f1cf6e4c7d6a02c1fb71
张璟璇 <jingxuangeoffrey@gmail.com>
2021-02-28 14:22:46 +0800

    Initial commit

---
+++ HelloWord/src/com/zjx/HelloWord.java
@@ -0,0 +1,11 @@
+package com.zjx;
+
+/**
+ * @author zjx
+ * @date 2021-02-28 - 10:47
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("HelloWord");
+    }
+}
