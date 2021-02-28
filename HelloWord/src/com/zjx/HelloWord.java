db6725c3e3ed04c4fbf334a4b006c0413cdde891
jingxuangeoffrey <77772876+jingxuangeoffrey@users.noreply.github.com>
2021-02-28 14:35:50 +0800

    Update HelloWord.java

--- HelloWord/src/com/zjx/HelloWord.java
+++ HelloWord/src/com/zjx/HelloWord.java
@@ -7,8 +7,5 @@ package com.zjx;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("HelloWord");
-        System.out.println("HelloWord");
-        System.out.println("看来是好了");
-        System.out.println("看来是好了");
     }
 }
