d56539d99f3fafcd23c4cb66eb1d589cd6cc6e9a
张璟璇 <jingxuangeoffrey@gmail.com>
2021-02-28 14:34:52 +0800

    第二次提交

--- HelloWord/src/com/zjx/HelloWord.java
+++ HelloWord/src/com/zjx/HelloWord.java
@@ -8,5 +8,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("HelloWord");
         System.out.println("HelloWord");
+        System.out.println("看来是好了");
     }
 }
